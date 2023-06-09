f8d5bbf07943b2f7ba76d5fb831974fbed576074
medhidev <grimalmedhi4@gmail.com>
2023-06-09 17:02:46 +0200

    refait interface

---
+++ Js/xp.js
@@ -0,0 +1,12 @@
+// window.addEventListener('load', function() {
+//   var text = document.getElementById('myText');
+//   var underline = document.getElementById('underline');
+
+//   text.addEventListener('mouseover', function() {
+//     underline.style.width = text.offsetWidth + 'px';
+//   });
+
+//   text.addEventListener('mouseout', function() {
+//     underline.style.width = '0';
+//   });
+// });
